1bd22f4315fb796d789cb9c321eef01f4824ff18
ian1017 <mdawidamengo@gmail.com>
2020-06-15 23:24:18 +0300

    register font awesome

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from config import config_options
+from flask_fontawesome import FontAwesome
+
+fa = FontAwesome
 
 def create_app(config_name):
     '''
@@ -15,6 +18,9 @@ def create_app(config_name):
     #creating the app configuration
     app.config.from_object(config_options[config_name])
 
+    #Initialize flask extensions
+    fa.init_app(app)
+    
     # Registering the main blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
